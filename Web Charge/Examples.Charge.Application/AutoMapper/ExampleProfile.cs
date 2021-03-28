1af91c7512e90ed4e02f3098a3dd99f3a05dece8
Thomaz Bandeira <thomaz@bandeira.com>
2021-03-28 17:35:21 -0300

    Ajuste do profile do mapper para o resto dos DTOs existentes no projeto.

--- Web Charge/Examples.Charge.Application/AutoMapper/ExampleProfile.cs
+++ Web Charge/Examples.Charge.Application/AutoMapper/ExampleProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Examples.Charge.Application.Dtos;
 using Examples.Charge.Domain.Aggregates.ExampleAggregate;
+using Examples.Charge.Domain.Aggregates.PersonAggregate;
 
 namespace Examples.Charge.Application.AutoMapper
 {
@@ -12,6 +13,17 @@ namespace Examples.Charge.Application.AutoMapper
                .ReverseMap()
                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome));
+
+            CreateMap<Person, PersonDto>()
+               .ReverseMap()
+               .ForMember(dest => dest.BusinessEntityID, opt => opt.MapFrom(src => src.BusinessEntityID))
+               .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<PersonPhone, PersonPhoneDto>()
+               .ReverseMap()
+               .ForMember(dest => dest.BusinessEntityID, opt => opt.MapFrom(src => src.BusinessEntityID))
+               .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+               .ForMember(dest => dest.PhoneNumberTypeID, opt => opt.MapFrom(src => src.PhoneNumberTypeID));
         }
     }
 }
